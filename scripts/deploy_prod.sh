28110f6d9764ad12c0261ce2fa6d23adc60a98cc
jake_p3 <jake@jkbx.com>
2023-02-13 15:27:25 -0800

    Initial VPC creation

---
+++ scripts/deploy_prod.sh
@@ -0,0 +1,7 @@
+sam package --region us-west-2 --template-file template.yaml \
+--s3-bucket infrastructure_prod \
+--s3-prefix api_builds --output-template-file packaged.yaml
+
+sam deploy --region us-west-2 --no-fail-on-empty-changeset --template-file packaged.yaml \
+--stack-name JKBX-Infra-Prod \
+--capabilities CAPABILITY_IAM --parameter-overrides StageName=nonprod
